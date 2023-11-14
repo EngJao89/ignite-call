d46cc094b9554aa68c171793ca464173975a110b
c1334690 <joao.ribeiro@capgemini.com>
2023-11-13 21:00:15 -0300

    feat(ic02-eslint-configs): implementa configurações de eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { Heading } from '@ignite-ui/react'
-import { Inter } from 'next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <Heading as="h1">Hello Ignite Call</Heading>
+  return ( 
+    <Heading as="h1">Hello Ignite Call</Heading> 
   )
 }
